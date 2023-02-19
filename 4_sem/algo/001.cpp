cc8d450c737bb905094069c1a4681a06e443228e
getylman <kanuch2003@gmail.com>
2023-02-19 16:20:26 +0300

    +dfs

--- 4_sem/algo/001.cpp
+++ 4_sem/algo/001.cpp
@@ -3,95 +3,29 @@
 #include <map>
 #include <set>
 #include <vector>
-// 1i
-class Solution {
-  enum Color { WHITE, GREY, BLACK };
-  struct Node;
-  std::vector<Node> adj_list_;
-  std::set<size_t> result_;
-  std::map<std::pair<size_t, size_t>, size_t> edge_num_;
-  struct Node {
-    Color color = Color::WHITE;
-    size_t value = 0;
-    std::set<size_t> neibors;
-    size_t t_in = size_t(-1);
-    size_t t_in_min = size_t(-1);
-    void AddNeib(const size_t& vertex) { neibors.insert(vertex); }
-  };
-  void BuildAdjList(
-      const size_t& vetrexes,
-      const std::vector<std::pair<std::pair<size_t, size_t>, size_t>>& edges) {
-    adj_list_.resize(vetrexes + 1);
-    for (size_t i = 1; i < adj_list_.size(); ++i) {
-      adj_list_[i].value = i;
-    }
-    for (const auto& i : edges) {
-      if (i.first.first == i.first.second) {
-        continue;
-      }
-      adj_list_[i.first.first].AddNeib(i.first.second);
-      adj_list_[i.first.second].AddNeib(i.first.first);
-      edge_num_.insert({{i.first.first, i.first.second}, i.second});
-      edge_num_.insert({{i.first.second, i.first.first}, i.second});
-    }
-  }
-  void Nothing() {}
-  void Cringe(Node*& cur_node, size_t& counter_of_time, const size_t& iter);
-  void AnotherCringe(Node*& cur_node, const size_t& iter) {
-    cur_node->t_in_min =
-        ((cur_node->t_in_min < adj_list_[iter].t_in) ? (cur_node->t_in_min)
-                                                     : (adj_list_[iter].t_in));
-  }
-  void DFS(Node* cur_node, size_t& counter_of_time,
-           size_t par_node = size_t(-1)) {
-    cur_node->color = Color::BLACK;
-    cur_node->t_in = counter_of_time;
-    cur_node->t_in_min = counter_of_time;
-    ++counter_of_time;
-    for (const auto& i : adj_list_[cur_node->value].neibors) {
-      (i != par_node) ? (adj_list_[i].color != Color::BLACK)
-                            ? (Cringe(cur_node, counter_of_time, i))
-                            : (AnotherCringe(cur_node, i))
-                      : (Nothing());
-    }
-  }
 
+class A {
  public:
-  void Answer(
-      const size_t& vertexes,
-      const std::vector<std::pair<std::pair<size_t, size_t>, size_t>>& edges) {
-    BuildAdjList(vertexes, edges);
-    size_t counter_of_time = 0;
-    for (auto& i : adj_list_) {
-      (i.color == Color::WHITE) ? DFS(&i, counter_of_time) : Nothing();
-    }
-    std::cout << result_.size() << '\n';
-    for (const auto& i : result_) {
-      std::cout << i << ' ';
-    }
+ struct B {
+  size_t a = 0, b = 1;
+ };
+ //friend class C;
+};
+
+class C {
+  void foo1() {
+    A::B a;
+  }
+  friend class D;
+};
+
+class D : public C {
+ public: 
+  void foo() {
+    A::B a;
   }
 };
 
-void Solution::Cringe(Node*& cur_node, size_t& counter_of_time,
-                      const size_t& iter) {
-  DFS(&adj_list_[iter], counter_of_time, cur_node->value);
-  cur_node->t_in_min = (cur_node->t_in_min < adj_list_[iter].t_in_min)
-                           ? cur_node->t_in_min
-                           : adj_list_[iter].t_in_min;
-  std::pair<size_t, size_t> tmp = {cur_node->value, adj_list_[iter].value};
-  (adj_list_[iter].t_in_min > cur_node->t_in)
-      ? ((void)result_.insert(edge_num_[tmp]))
-      : (Nothing());
-}
-
 int main() {
-  size_t vertexes = 0;
-  size_t num_of_edges = 0;
-  std::cin >> vertexes >> num_of_edges;
-  std::vector<std::pair<std::pair<size_t, size_t>, size_t>> edges(num_of_edges);
-  for (size_t i = 0; i < num_of_edges; ++i) {
-    std::cin >> edges[i].first.first >> edges[i].first.second;
-    edges[i].second = i + 1;
-  }
-  Solution().Answer(vertexes, edges);
+  
 }
--- 4_sem/algo/002.cpp
+++ 4_sem/algo/002.cpp
@@ -32,6 +32,7 @@ template <typename VertexT = size_t,
           typename EdgeT = std::pair<VertexT, VertexT>, bool Directed = true>
 class ALGraph : public Graph<VertexT, EdgeT> {
   // adj list
+ public:
   enum Color { WHITE, GRAY, BLACK };
   struct NodeEdge {
     EdgeT value;                             // значение ребра
@@ -50,6 +51,8 @@ class ALGraph : public Graph<VertexT, EdgeT> {
     std::unordered_map<VertexT, std::vector<NodeEdge>>
         neighbours;                 // для случая с кратными ребрами
   };
+
+ private:
   NodeEdge SetNodeEdge(
       const EdgeT& value, const size_t& number,
       const std::pair<size_t, size_t>& connection,
@@ -74,8 +77,9 @@ class ALGraph : public Graph<VertexT, EdgeT> {
  public:
   using vertex_type = VertexT;
   using edge_type = EdgeT;
-  ALGraph(const size_t& num_of_vertexes,
-          const std::vector<EdgeT>& edges)
+  using vertex_node_type = NodeVertex;
+  using edge_node_type = NodeEdge;
+  ALGraph(const size_t& num_of_vertexes, const std::vector<EdgeT>& edges)
       : num_of_edges_(edges.size()),
         num_of_vertexes_(num_of_vertexes),
         edges_(edges) {  // конструктор графа от вектора ребер
@@ -128,54 +132,102 @@ class ALGraph : public Graph<VertexT, EdgeT> {
         if (graph_.count(tmp_edge.second) != 0) {
           if (graph_[tmp_edge.second].neighbours.count(tmp_edge.first) != 0) {
             graph_[tmp_edge.second].neighbours[tmp_edge.first].push_back(
-                SetNodeEdge(i, ++counter_edge,
+                SetNodeEdge(i, counter_edge,
                             {tmp_edge.second, tmp_edge.first}));
           } else {
             graph_[tmp_edge.second].neighbours.insert(
                 {tmp_edge.first,
-                 {SetNodeEdge(i, ++counter_edge,
+                 {SetNodeEdge(i, counter_edge,
                               {tmp_edge.second, tmp_edge.first})}});
           }
         } else {
           tmp_node_vertex.value = tmp_edge.second;
-          tmp_node_vertex.number = ++counter_vertex;
+          tmp_node_vertex.number = counter_vertex;
           tmp_node_vertex.neighbours.insert(
               {tmp_edge.first,
-               {SetNodeEdge(i, ++counter_edge,
+               {SetNodeEdge(i, counter_edge,
                             {tmp_edge.second, tmp_edge.first})}});
           graph_.insert({tmp_edge.first, tmp_node_vertex});
         }
       }
     }
   }
-  size_t NumOfVertexes() const final { return graph_.size(); }
-  size_t NumOfEdges() const final { return num_of_edges_; }
+  size_t NumOfVertexes() const final {
+    return graph_.size();
+  }
+  size_t NumOfEdges() const final {
+    return num_of_edges_;
+  }
   std::vector<VertexT*>& GetNeighbours(const VertexT& vertex) final {
     return graph_[vertex];
   }
   std::unordered_map<VertexT, NodeEdge>::iterator& GetIteratorOfNeighboursList(
-      const VertexT& vertex) { return graph_[vertex].neighbours.begin(); }
+      const VertexT& vertex) {
+    return graph_[vertex].neighbours.begin();
+  }
+  std::unordered_map<VertexT, NodeEdge>& GetBody() {
+    return graph_;
+  }
 };
 
 template <typename VertexT, typename EdgeT>
 class Visitor {
  public:
-  virtual void DiscoverVertex(VertexT&, Graph<VertexT, EdgeT>&) = 0;
+  // virtual std::vector<size_t> DiscoverVertex(
+  //     VertexT& vertex, ALGraph<VertexT, EdgeT, false>& graph) = 0;
   virtual ~Visitor() = default;
 };
 
 template <typename VertexT, typename EdgeT>
 class DFS : public Visitor<VertexT, EdgeT> {
+ private:
+  using graph_type = ALGraph<VertexT, EdgeT, false>;
+  struct DFSVertexNode {
+    graph_type::NodeVertex* cur_node;
+    graph_type::NodeVertex* prev_node = nullptr;
+    DFSVertexNode(const graph_type::NodeVertex*& cur,
+                  const graph_type::NodeVertex*& prev)
+        : cur_node(cur), prev_node(prev) {}
+    DFSVertexNode(const DFSVertexNode& node)
+        : cur_node(node.cur_node), prev_node(node.prev_node) {}
+  };
+  // переделать без рекурсии
+  void operator()(ALGraph<VertexT, EdgeT, false>& graph, DFSVertexNode node,
+                  std::vector<size_t>& bridges) {
+    node.cur_node->color = graph_type::Color::BLACK;
+    node.cur_node->t_in = counter_of_time_;
+    node.cur_node->t_in_min = counter_of_time_;
+    ++counter_of_time_;
+    for (const auto& i : graph.GetBody()[node.cur_node->value].neighbours) {
+      if (node.prev_node != nullptr and node.prev_node->value == i.first) {
+        if (node.cur_node->color == graph_type::Color::BLACK) {
+          node.cur_node->t_in_min =
+              std::min(node.cur_node->t_in_min, graph.GetBody()[i.first].t_in);
+        } else {
+          operator()(graph, DFSVertexNode(&graph.GetBody()[i.first], node.cur_node), bridges);
+          node.cur_node->t_in_min = std::min(node.cur_node->t_in_min, graph.GetBody()[i.first].t_in_min);
+          if (graph.GetBody()[i.first].t_in_min > node.cur_node->t_in) {
+            bridges.push_back(i.second.front().number);
+          }
+        }
+      }
+    }
+  }
+
  public:
   
+
  private:
-   
+  size_t counter_of_time_ = 0;
+
+  graph_type graph_;
+  // std::vector<ALGraph<VertexT, EdgeT, false>::NodeEdge> result_;
+  // верхний прикол не сработал и решил использовать просто номера ребер
+  std::vector<size_t> result_;
 };
 
 template <typename VertexT, typename EdgeT>
-class Solution {
-
-};
+class Solution {};
 
 int main() {
 }
