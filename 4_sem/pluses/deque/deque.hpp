689267548d36108ad87da7b4c5be5931f3ebf2a2
getylman <kanuch2003@gmail.com>
2023-02-22 15:07:11 +0300

    + method definition

--- 4_sem/pluses/deque/deque.hpp
+++ 4_sem/pluses/deque/deque.hpp
@@ -10,7 +10,35 @@
 template <typename T, typename Allocator = std::allocator<T>>
 class Deque {
  public:
-
+  //=================Constructors=================
+  Deque();
+  Deque(const size_t& count);
+  Deque(const size_t& count, const T& value);
+  Deque(const Deque<T, Allocator>& deq);
+  ~Deque();
+  Deque<T, Allocator>& operator=(cosnt Deque<T, Allocator>& deq);
+  //==============================================
+  //=============Accessing an element=============
+  T& operator[](const size_t& index);
+  const T& operator[](const size_t& index) const;
+  T& at(const size_t& index);
+  const T& at(const size_t& index) const;
+  //==============================================
+  //==============SizeInformation=================
+  size_t size() const { return total_size_; }
+  bool empty() const { return size() == 0; }
+  //==============================================
+  //===========Modification methods===============
+  void push_back(const T& elem);
+  void pop_back();
+  void push_front(const T& elem);
+  void pop_front();
+  //==============================================
+  //==================ITERATOR====================
+  struct iterator {
+    
+  };
+  //==============================================
  private:
   const uint64_t kChunkSize = 0; // size of chunks
   uint64_t total_size_ = 0; // size of in full container
